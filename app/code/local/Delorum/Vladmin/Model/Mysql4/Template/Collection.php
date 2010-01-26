be29749f13049e429825d12575b0ad756622653b
Tyler Flint <tylerflint@Delorum10.local>
2010-01-25 22:45:20 -0700

    removing vladmin module to prepare for pear package install

--- app/code/local/Delorum/Vladmin/Model/Mysql4/Template/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Delorum_Vladmin_Model_Mysql4_Template_Collection extends Mage_Eav_Model_Entity_Collection_Abstract
-{
-	public function _construct()
-    {
-        $this->_init('vladmin/template');
-    }
-}
