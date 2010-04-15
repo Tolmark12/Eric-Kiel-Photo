d93c478717dfc76c54532bde14ee0bdc3b4470ef
Justin Pratt <justin@justin-pratts-macbook-2.local>
2010-04-15 09:36:26 -0600

    NEW: changed post fields coming into controller

--- app/code/local/Delorum/Client/controllers/IndexController.php
+++ app/code/local/Delorum/Client/controllers/IndexController.php
@@ -4,10 +4,7 @@ class Delorum_Client_IndexController extends Mage_Core_Controller_Front_Action
 	public function postDataAction()
 	{
 		$data = $this->getRequest()->getParams();
-		$datafield = "";
-		foreach($data as $field) {
-			$datafield .= $field . "|";
-		}
+		$formData = $data['formData'];
 		$type = $data['formtype'];
 		$data['created_at'] = now();
 		$data['updated_at'] = now();
@@ -15,16 +12,10 @@ class Delorum_Client_IndexController extends Mage_Core_Controller_Front_Action
 			$client = Mage::getModel('client/client');
 			$client->setId(null);
 			$client->setFormType($type);
-			$client->setDataField($datafield);
+			$client->setDataField($formData);
 			$client->setCreatedAt($data['created_at']);
 			$client->setUpdatedAt($data['updated_at']);
 			$client->save();
-			if(isset($data['name'])) {
-				$data['emailname'] = $data['name'];
-			}
-			if(isset($data['email'])) {
-				$data['emailemail'] = $data['email'];
-			}
 			Mage::getModel('core/email_template')
 				->sendTransactional(
 					2,
