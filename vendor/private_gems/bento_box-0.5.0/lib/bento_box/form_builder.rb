b672397872294f07c814d7dcb34bedccbc5fef1e
Ryan Dew <ryandew@Ryan-Dews-MacBook-Pro.local>
2011-06-07 18:29:50 -0600

    Fix breadcrumb

--- vendor/private_gems/bento_box-0.5.0/lib/bento_box/form_builder.rb
+++ vendor/private_gems/bento_box-0.5.0/lib/bento_box/form_builder.rb
@@ -17,11 +17,14 @@ module BentoBox
       relation_object    = eval(relation_singular.classify)
       options[:label]   ||= relation_object.respond_to?(:name) ? :name : relation_object.respond_to?(:title) ? :title : :id
       
-      selector_tag = link_to("",%{#{Rails.application.routes.url_helpers.bento_selector_path(:type => (options[:type] || "default"), :object => object_class_str, 
+      selector_tag = link_to("",Rails.application.routes.url_helpers.bento_selector_path(:type => (options[:type] || "default"), :object => object_class_str, 
                   :attribute => relation_singular, :label => options[:label], :is_label_image => options[:is_label_image],
                   :id => object.id, :orderable => options[:orderable],
-                  :destination => "#{object_class_str}-#{relation.to_s}")}},
-                                  :rel =>"address:/#{relation_singular}", :class => "selector-link")
+                  :destination => "#{object_class_str}-#{relation.to_s}"),
+                                  :rel =>"address:/#{Rails.application.routes.url_helpers.bento_selector_path(:type => (options[:type] || "default"), :object => object_class_str, 
+                  :attribute => relation_singular, :label => options[:label], :is_label_image => options[:is_label_image],
+                  :id => object.id, :orderable => options[:orderable],
+                  :destination => "#{object_class_str}-#{relation.to_s}")}", :class => "selector-link")
       selector_tag << @template.submit_tag("Select #{relation.to_s.titleize}", :class => 'button-thin select-items', :onclick => '$(this).prev("a.selector-link").click(); return false;')
       selector_tag << "<div id='#{object_class_str}-#{relation.to_s}'>".html_safe
       object.send(relation).each do |action|
